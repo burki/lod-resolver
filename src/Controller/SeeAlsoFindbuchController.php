0aaccfab18b66d83cbcc4f95d7cf792dd1e61429
Daniel Burckhardt <daniel.burckhardt@sur-gmbh.ch>
2024-04-02 11:05:07 +0200

    http://beacon.findbuch.de/ no longer works, therefore always return 404

--- src/Controller/SeeAlsoFindbuchController.php
+++ src/Controller/SeeAlsoFindbuchController.php
@@ -11,8 +11,9 @@ use Symfony\Contracts\HttpClient\HttpClientInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;;
 
 /**
- * Simple pass-through proxy since https://beacon.findbuch.de/ has an
- * expired certificate
+ * http://beacon.findbuch.de/ no longer works.
+ * 
+ * Therefore always return 404
  *
  * @Route("/seealso/findbuch")
  */
@@ -21,44 +22,11 @@ extends AbstractController
 {
     const FINDBUCH_SEEALSO_URL = 'http://beacon.findbuch.de';
 
-    private $client;
-
-    public function __construct(HttpClientInterface $client)
-    {
-        $this->client = $client;
-    }
-
     /**
      * @Route("{path}", requirements={"path": ".+"}, name="findbuch-proxy")
      */
     public function seeAlsoFindbuchAction(Request $request, $path)
     {
-        $url = self::FINDBUCH_SEEALSO_URL . $path;
-
-        $queryString = http_build_query($request->query->all());
-        if (!empty($queryString)) {
-            $url .= '?' . $queryString;
-        }
-
-        $clientResponse = $this->client->request('GET', $url);
-
-        // Responses are lazy: this code is executed as soon as headers are received
-        if (200 !== $clientResponse->getStatusCode()) {
-            throw new \Exception($url . ' could not be fetched');
-        }
-
-        $response = new StreamedResponse();
-
-        $contentType = $clientResponse->getHeaders()['content-type'][0];
-        $response->headers->set('Content-Type', $contentType);
-
-        $response->setCallback(function () use ($clientResponse) {
-            foreach ($this->client->stream($clientResponse) as $chunk) {
-                echo $chunk->getContent();
-                flush();
-            }
-        });
-
-        return $response;
+        throw $this->createNotFoundException(self::FINDBUCH_SEEALSO_URL . ' no longer works');
     }
 }
