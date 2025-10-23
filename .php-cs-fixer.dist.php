ee319bd81ee0aad840199635c44564cfc4cee690
Daniel Burckhardt <daniel.burckhardt@sur-gmbh.ch>
2025-10-23 19:13:58 +0200

    rename visibility_required into modifier_keywords add 'operator_linebreak' => ['only_booleans' => true]

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -16,6 +16,11 @@ return (new PhpCsFixer\Config())
         '@PHP82Migration' => true,
         'control_structure_continuation_position' => ['position' => 'next_line'],
         'elseif' => false, // don't change else if to elseif
+        'operator_linebreak' => ['only_booleans' => true],
+        'modifier_keywords' => ['elements' =>
+            // disable changing var into public
+            [/*'const', 'method', 'property'*/]
+        ],
     ])
     ->setFinder($finder)
 ;
